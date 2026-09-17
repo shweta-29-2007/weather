096c33c351dd485873df13c048fa7d739730d798
shweta <shwetashingri2@gmail.com>
2026-09-17 13:51:39 +0530

    Initial commit

---
+++ weather.py
@@ -0,0 +1,8 @@
+degree=(int(input("enter the degree: "))
+        if degree <=20:
+        
+            print("cold wheather.")
+        elif degree >=20 and degree <=38:
+            print("normal weather.")
+        else:
+            print("HOT! weather.")
